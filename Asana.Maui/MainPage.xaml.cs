bcb91a41e4eff80889e3e83e83d5200faade7dbc
Julissa Su <julissa.su01@gmail.com>
2025-06-30 02:54:56 -0400

    Update MainPage.xaml.cs

--- Asana.Maui/MainPage.xaml.cs
+++ Asana.Maui/MainPage.xaml.cs
@@ -1,14 +1,24 @@
-﻿using Asana.Maui.ViewModels;
+﻿using Asana.Library.Models;
+using Asana.Maui.ViewModels;
 
 namespace Asana.Maui;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	private MainPageViewModel _viewModel;
 
 	public MainPage()
 	{
 		InitializeComponent();
-		BindingContext = new MainPageViewModel();
+		_viewModel = new MainPageViewModel();
+		BindingContext = _viewModel;
+	}
+
+	private void OnToDoCompleteChanged(object sender, CheckedChangedEventArgs e)
+	{
+		if (sender is CheckBox checkBox && checkBox.BindingContext is ToDo toDo)
+		{
+			_viewModel.ToggleToDoCompleteCommand.Execute(toDo);
+		}
 	}
 }
